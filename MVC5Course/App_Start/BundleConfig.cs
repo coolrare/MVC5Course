b8c8ee01f2c3e54718beb28857b6622305a8fd78
Will <doggy.huang@gmail.com>
2018-07-07 01:41:28 +0800

    加入專案檔案。

---
+++ MVC5Course/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MVC5Course
+{
+    public class BundleConfig
+    {
+        // 如需統合的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
+            // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
